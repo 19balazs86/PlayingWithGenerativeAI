46649fd4d041a1324401cd62be6750fdbfabde74
Balazs Kereszturi <19balazs86@gmail.com>
2024-07-15 11:48:10 +0200

    Add logging to the example

--- ConsoleAppSK/Examples/KernelInvokePrompt.cs
+++ ConsoleAppSK/Examples/KernelInvokePrompt.cs
@@ -1,16 +1,28 @@
-﻿using Microsoft.SemanticKernel;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
 
 namespace ConsoleAppSK.Examples;
+
 public static class KernelInvokePrompt
 {
-    public static async Task Run(string prompt)
+    public static async Task Run(string prompt, bool enableLogging = false)
     {
-        IKernelBuilder builder = Kernel.CreateBuilder();
+        IKernelBuilder builder      = Kernel.CreateBuilder();
+        IServiceCollection services = builder.Services;
 
         // builder.AddAzureOpenAIChatCompletion(...); // AzureOpenAI
         // builder.AddOpenAIChatCompletion(...); // This works the same as builder.Services.AddOpenAI
 
-        builder.Services.AddOpenAIChatCompletion(OpenAIConfig.Models.GPT_3_5_turbo, OpenAIConfig.ApiKey);
+        // Add services to the container
+        {
+            services.AddOpenAIChatCompletion(OpenAIConfig.Models.GPT_3_5_turbo, OpenAIConfig.ApiKey);
+
+            if (enableLogging)
+            {
+                services.AddLogging(loggingBuilder => loggingBuilder.AddConsole().SetMinimumLevel(LogLevel.Trace));
+            }
+        }
 
         Kernel kernel = builder.Build();
 
--- ConsoleAppSK/Program.cs
+++ ConsoleAppSK/Program.cs
@@ -6,6 +6,6 @@ public static class Program
 {
     public static async Task Main(string[] args)
     {
-        await KernelInvokePrompt.Run("Who are you?");
+        await KernelInvokePrompt.Run("Who are you?", enableLogging: false);
     }
 }
