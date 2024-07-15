6d92d5b6329607ce0b9bdbf4de36328284d91e38
Balazs Kereszturi <19balazs86@gmail.com>
2024-07-15 17:31:47 +0200

    Add new project WebApiSK

---
+++ WebApiSK/Endpoints/WeatherForecastEndpoints.cs
@@ -0,0 +1,35 @@
+﻿namespace WebApiSK.Endpoints;
+
+public static class WeatherForecastEndpoints
+{
+    public static void MapWeatherForecastEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("/weatherforecast", getWeatherForecast);
+    }
+
+    private static async Task<WeatherForecast> getWeatherForecast()
+    {
+        await Task.Delay(500);
+
+        return new WeatherForecast
+        {
+            Date         = DateOnly.FromDateTime(DateTime.Now),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary      = WeatherForecast.Summaries[Random.Shared.Next(WeatherForecast.Summaries.Length)]
+        };
+    }
+}
+
+public sealed class WeatherForecast
+{
+    public static readonly string[] Summaries =
+    [
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    ];
+
+    public required DateOnly Date { get; init; }
+
+    public required int TemperatureC { get; init; }
+
+    public required string Summary { get; init; }
+}
---
+++ WebApiSK/Program.cs
@@ -0,0 +1,26 @@
+using WebApiSK.Endpoints;
+
+namespace WebApiSK;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        IServiceCollection services   = builder.Services;
+
+        // Add services to the container
+        {
+
+        }
+
+        WebApplication app = builder.Build();
+
+        // Configure the HTTP request pipeline
+        {
+            app.MapWeatherForecastEndpoints();
+        }
+
+        app.Run();
+    }
+}
